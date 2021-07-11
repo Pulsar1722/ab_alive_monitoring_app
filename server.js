f6479cdcb0ea158f71407c487d940a8fff55275a
HaruMiya1722 <mebe889@kagi.be>
2021-07-11 18:28:43 +0900

    試験前。ひとまず完成?

--- server.js
+++ server.js
@@ -5,24 +5,25 @@
 'use strict';
 
 //各種パラメータ(Webページ死活監視)
-const CRON_INTERVAL_MINUTE = 10; //cronによる定期実行の間隔(分)
+const CRON_EVERY_MINUTE = 10; //cronによる定期実行の時間指定(分)
 const aliveMonitoredURL = [ //死活監視対象URLのリスト
-    "https://kusuri-miru.com/",
+    `https://kusuri-miru.com/`,
 ];
-const MAX_REQUEST_TRY_TIME = 3; //1回の死活監視における最大試行回数
+const MAX_REQUEST_TRY_TIMES = 3; //1回の死活監視における最大リクエスト試行回数
+const REQUEST_TRY_INTERVAL_MS = 3 * 1000; //リクエスト試行のインターバル(単位:ms)
 const MAX_REQURST_TIMEOUT_MS = 10 * 1000; //タイムアウト(単位:ms)
 
 //各種パラメータ(通知送信)
 const send_mail_addrs = [ //通知メールの宛先アドレスのリスト
-    "mebe889@kagi.b",
+    `mebe889@kagi.be`,
 ];
 
 //共通パラメータ
-const APP_NAME = "alive_mon"; //本アプリ名
+const APP_NAME = `alive_mon`; //本アプリ名
 const APP_VERSION = class {
-    static major = "1";
-    static minor = "0";
-    static revision = "0";
+    static major = `1`;
+    static minor = `0`;
+    static revision = `0`;
 }
 
 //Webページ死活監視結果の通知内容のオブジェクト
@@ -34,7 +35,8 @@ function webPageAliveMonitoringDetail(isAlive, url, HTTPStatusCode) {
 
 //使用モジュール
 const cron = require('node-cron');
-const request = require('request');
+const axios = require('axios');
+require('date-utils');
 
 //このファイルがメインモジュールかの確認に用いるらしい
 if (require.main === module) {
@@ -42,18 +44,20 @@ if (require.main === module) {
 }
 
 function main() {
-    generateLog("AppVersion: " + APP_VERSION.major + "." + APP_VERSION.minor + "." + APP_VERSION.revision);
+    generateLog(`AppVersion: ${APP_VERSION.major}.${APP_VERSION.minor}.${APP_VERSION.revision}`);
 
     try {
         //アプリ起動直後は即座に1回実行
+        generateLog(new Date().toFormat(`YYYYMMDDHH24MISS`));
         aliveMonitoringHandler();
 
         // cronによる周期実行
-        cron.schedule('*/' + CRON_INTERVAL_MINUTE + ' * * * *', () => {
+        cron.schedule(`*/${CRON_EVERY_MINUTE} * * * *`, () => {
+            generateLog(new Date().toFormat(`YYYYMMDDHH24MISS`));
             aliveMonitoringHandler();
         });
     } catch (error) {
-        generateErrLog(error);
+        generateErrLog(JSON.stringify(error))
     }
 }
 
@@ -62,12 +66,12 @@ function main() {
  * @classdesc 死活監視処理を呼び出す関数
  */
 function aliveMonitoringHandler() {
-    generateLog("Entered aliveMonitoringHandler()");
+    generateLog(`Entered aliveMonitoringHandler(${JSON.stringify(arguments)})`);
 
     //Webページ死活監視処理
-    aliveMonitoredURL.forEach(url => {
+    aliveMonitoredURL.forEach(async (url) => {
         let ret;
-        ret = webPageAliveMonitoring(url);
+        ret = await webPageAliveMonitoring(url);
         if (!ret.isAlive) {
             sendErrNotice(ret);
         }
@@ -79,37 +83,44 @@ function aliveMonitoringHandler() {
  * @param {string} url 死活監視対象のWebページURL
  * @return {webPageAliveMonitoringDetail} 通知内容
  */
-function webPageAliveMonitoring(url) {
-    generateLog(`Entered webPageAliveMonitoring()`);
+async function webPageAliveMonitoring(url) {
+    generateLog(`Entered webPageAliveMonitoring(${JSON.stringify(arguments)})`);
 
     let isAlive = false;
-    let HTTPResCode = 0;
+    let HTTPResCode = "N/A";
+    let res = null;
 
-    const options = {
+    const config = {
         url: url,
+        method: "GET",
         timeout: MAX_REQURST_TIMEOUT_MS,
     }
 
     //最大「MAX_REQUEST_TRY_TIME」回試行
-    for (let i = 0; i < MAX_REQUEST_TRY_TIME; i++) {
-        //URLにGETリクエスト送信
-        request(options, (error, response, body) => {
-            // レスポンスコードとHTMLを表示
-            generateLog(`url: ${url}`);
-            generateLog(`statusCode: ${response} ${response.statusCode}`);
-            //generateLog(`body: ${ body }`);
-            HTTPResCode = response.statusCode;
+    for (let i = 0; i < MAX_REQUEST_TRY_TIMES; i++) {
+        try {
+            //リクエストの送信とレスポンスの受信(同期的)
+            res = await sendRequestSync(config);
+            HTTPResCode = res.status;
 
             //ステータスコードは200のみを正常とする
-            if (HTTPResCode !== 200) {
-                generateErrLog(url + ' request(GET) error:' + error);
+            if (res.status !== 200) {
+                generateErrLog(`${url} request[GET] error(${res.status})`);
                 isAlive = false;
             } else {
                 isAlive = true;
             }
-        });
+        } catch (error) {
+            HTTPResCode = "";
+            generateErrLog(error);
 
-        //TBD request()処理が終了する前にここの処理に来てしまうのはどうすればよいのか
+            //現状の仕様では、HTTPResCodeにエラーメッセージも入れちゃう
+            if (error.response) {
+                HTTPResCode = error.response.status;
+            }
+            HTTPResCode += `\n${error}`
+        }
+
         if (isAlive) {
             //1回でも正常判定なら抜ける
             break;
@@ -117,23 +128,6 @@ function webPageAliveMonitoring(url) {
     }
 
     return new webPageAliveMonitoringDetail(isAlive, url, HTTPResCode);
-}
-
-/**
- * @classdesc Promiseを返すリクエスト処理
- * @param detail requestに渡すオプション
- * @return requestの実行結果
- */
-function requestPromise(options) {
-    return new Promise((resolve, reject) => {
-        request(options, (error, response, body) => {
-            if (error) {
-                reject("Promise: エラー");
-            } else {
-                resolve("Promise: 正常");
-            }
-        });
-    });
 }
 
 /**
@@ -165,14 +159,18 @@ function generateNoticeMailContents(detail) {
         user: 'ab.robomail@gmail.com',
         pass: 'X2wvCNRH',
         //to: //後で指定する
-        subject: "<" + APP_NAME + "> Webページ異常応答！！！",
+        subject: `< ${APP_NAME}> Webページ異常応答！！！`,
         text:
-            "以下のURLのwebページにおいて、異常応答を検出しました。\n" +
-            "\n" +
-            "URL: " + detail.url + "\n" +
-            "HTTPステータスコード: " + detail.HTTPResCode + "\n" +
-            "\n" +
-            "管理者は、必要に応じて対象Webページのサーバ再起動等を実施してください。\n",
+            `以下のURLのwebページにおいて、異常応答を検出しました。\n` +
+            `\n` +
+            `URL: ${detail.url} \n` +
+            `HTTPステータスコード: ${detail.HTTPResCode} \n` +
+            `\n` +
+            `管理者は、必要に応じて対象Webページのサーバ再起動等を実施してください。\n` +
+            `\n` +
+            `\n` +
+            `AppVersion: ${APP_VERSION.major}.${APP_VERSION.minor}.${APP_VERSION.revision} `,
+
     });
 
     return mailContent;
@@ -185,19 +183,20 @@ function generateNoticeMailContents(detail) {
  * @return {boolean} 送信成功ならtrue、送信失敗ならfalse
  */
 async function sendMail(destAddr, mailContent) {
-    generateLog("Entered sendMail() ");
+    generateLog(`Entered sendMail(${JSON.stringify(arguments)})`);
     let isOK = false;
 
     try {
         const { result, fullresult } = await mailContent(
             {
-                to: destAddr, //ここでtoが指定されるのをトリガーに、メールを送信する
+                to: destAddr, //ここでtoが指定されるのをトリガーに、メールを送信する。(なんでこんなAPI仕様なんだよ！)
             }
         )
-        generateLog("gmail-send result: " + result);
+        generateLog(`gmail - send result: ${result} `);
         isOK = true;
     } catch (error) {
-        generateErrLog("gmail-send ERROR: " + error);
+        generateErrLog(`gmail - send ERROR: ${error} `);
+        isOK = false;
     }
 
     return isOK;
@@ -214,7 +213,7 @@ async function sendMail(destAddr, mailContent) {
  * @return none
  */
 function generateLog(logstr) {
-    console.log("<" + APP_NAME + ">" + logstr);
+    console.log(`<${APP_NAME}> ${logstr}`);
 }
 
 /**
@@ -223,5 +222,14 @@ function generateLog(logstr) {
  * @return none
  */
 function generateErrLog(logstr) {
-    console.log("<" + APP_NAME + ">" + logstr);
+    console.error(`<${APP_NAME}> ${logstr}`);
+}
+
+/**
+ * @classdesc 同期的にリクエストを送信する関数
+ * @param config Request Config
+ * @return axiosのresponse
+ */
+async function sendRequestSync(config) {
+    return await axios(config);
 }
