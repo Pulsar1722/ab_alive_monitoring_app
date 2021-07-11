ee8936b910269120f0aa3ee03b1811536070a9cf
HaruMiya1722 <mebe889@kagi.be>
2021-07-11 21:07:23 +0900

    メアド追加

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const MAX_REQURST_TIMEOUT_MS = 10 * 1000; //タイムアウト(単位:ms)
 //各種パラメータ(通知送信)
 const dest_mail_addrs = [ //通知メールの宛先アドレスのリスト
     `mebe889@kagi.be`,
+    `rgstsvc@gmail.com`,
 ];
 const src_mail_info = { //送信元メールアドレス情報
     addr: `ab.robomail@gmail.com`,
